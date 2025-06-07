5cd3a62b6a38d9aa4d2f991e942dcdea1255a828
ravikmr0 <mbravikumar0@gmail.com>
2025-06-07 05:46:11 +0000

    all set

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
+import routes from "tempo-routes";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Project from "./pages/Project";
@@ -17,12 +17,17 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+const TempoRoutes = () => {
+  return useRoutes(routes);
+};
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        {import.meta.env.VITE_TEMPO && <TempoRoutes />}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
@@ -34,6 +39,7 @@ const App = () => (
           <Route path="/emi-options" element={<EMIOptions />} />
           <Route path="/gated-community" element={<GatedCommunity />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {import.meta.env.VITE_TEMPO && <Route path="/tempobook/*" />}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
